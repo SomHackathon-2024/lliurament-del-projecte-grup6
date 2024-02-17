4ce3a500a31b15f8aaa6687d679b35654d903f47
adecorte <adecorte@edu.tecnocampus.cat>
2024-02-17 19:09:38 +0100

    backend

--- hackathonGrup6/src/main/java/com/backend/hackathon/persistance/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.backend.hackathon.persistance;
-
-import com.backend.hackathon.domain.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<Users, String> {
-}
